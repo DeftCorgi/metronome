4047405f7809909b7debd2580bcea377732bac29
DeftCorgi <toanau123@gmail.com>
2018-07-10 18:22:54 +1000

    added example api

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  state = { cats: [] };
+
+  async componentDidMount() {
+    // load cats
+    const res = await axios.get('/api/cats');
+    const cats = res.data.cats;
+    this.setState({ cats });
+  }
+
   render() {
+    const cats = this.state.cats;
     return (
       <div className="App">
         <h1>App</h1>
+        {cats.map(cat => <div key={cat}>{cat}</div>)}
       </div>
     );
   }
