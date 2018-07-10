b1c166e8bb38e32a3ee8be351d9f137176fa7851
DeftCorgi <toanau123@gmail.com>
2018-07-10 14:47:12 +1000

    added route for example api

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 
 // middlewares
-app.use(express.static);
+app.use(express.static('./client/build'));
 
+// routes
+const router = express.Router();
+
+const catsRoutes = require('./routes/cats');
+router.use('/cats', catsRoutes);
+app.use('/api', router);
+
+// start server
 app.listen(process.env.PORT || 5000);
